6d909cbdd8cd0de2da120a160f1960693aa08afa
Segun <segunolalive@gmail.com>
2018-07-15 15:02:59 +0100

    rename app to picly

--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,10 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils import timezone
 from django.utils.text import slugify
 
-from lolu import settings
+from picly import settings
 
 import os.path
 
